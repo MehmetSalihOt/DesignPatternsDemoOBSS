ade4840a7eb7b072ee7416b96d6e1548b5f6f506
nurettinyakit <onuryakit@gmail.com>
2015-06-02 01:36:15 +0300

    Chain of Responsibility

---
+++ ChainOfResponsibility/Parser/TextParser.java
@@ -0,0 +1,18 @@
+public class TextParser extends Parser{
+ 
+  public TextParser(Parser successor){
+    this.setSuccessor(successor);
+  }
+   
+  @Override
+  public void parse(String fileName) {
+    if ( canHandleFile(fileName, ".txt")){
+      System.out.println("A text parser is handling the file: "+fileName);
+    }
+    else{
+      super.parse(fileName);
+    }
+     
+  }
+ 
+}
